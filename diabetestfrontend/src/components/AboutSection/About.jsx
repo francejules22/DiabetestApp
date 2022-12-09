9d699fad59142bea6766bf57a79b8fce625a0178
Francis Jules C. Espartero <80078725+francejules22@users.noreply.github.com>
2022-12-09 09:39:43 +0800

    Create About.jsx

---
+++ diabetestfrontend/src/components/AboutSection/About.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Sidebar from '../components/Sidebar/Sidebar';
+import Navbar from '../components/NavBar/NavBar';
+import HeroSection from '../components/HomeSection/Hero';
+import AboutSection from '../components/AboutSection/About';
+import ProcessSection from '../components/ProcessSection/Process';
+import TipSection from '../components/Tips/Tips';
+import TestSection from '../components/TestSection/Test';
+import Footer from '../components/Footer/Footer';
+
+
+const Home = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    };
+
+    return(
+        <>
+            <Sidebar isOpen={isOpen} toggle={toggle}/>
+            <Navbar toggle={toggle}/>
+            <HeroSection />
+            <AboutSection />
+            <ProcessSection />
+            <TestSection />
+            <TipSection />
+            <Footer />
+        </>
+    );
+}
+export default Home;
